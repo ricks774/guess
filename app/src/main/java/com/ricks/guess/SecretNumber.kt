10301419bd4562f81a574530b7897a415101aa13
Ricks <ricks774@gmail.com>
2022-10-13 18:33:57 +0800

    homework design

--- app/src/main/java/com/ricks/guess/SecretNumber.kt
+++ app/src/main/java/com/ricks/guess/SecretNumber.kt
@@ -3,12 +3,17 @@ package com.ricks.guess
 import java.util.Random
 
 class SecretNumber{
-    val secret = Random().nextInt(10) + 1   // 設定隨機亂數
+    var secret = Random().nextInt(10) + 1   // 設定隨機亂數
     var count = 0   // 紀錄數數
 
     fun vaildate(number : Int) : Int{
         count ++
         return number - secret
+    }
+
+    fun  reset(){
+        secret = Random().nextInt(10) + 1
+        count = 0
     }
 }
 
